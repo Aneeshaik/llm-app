e17ebd2d04ba3c93ce774db0c2fbe31724bdce59
Aneeshaik <aneesnafeesimmu@gmail.com>
2024-10-13 23:38:07 +0530

    side-bar-done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex h-screen flex-col">
       <Navbar />
+      <div className="flex flex-grow justify-start">
+        <Sidebar />
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,39 @@
+import inputLogo from "../assets/images/input-logo.png"
+import llmLogo from "../assets/images/llm-logo.png"
+import outputLogo from "../assets/images/output-logo.png"
+import threeLines from "../assets/images/three-lines.png"
+
+const Sidebar = () => {
+    return (
+        <div className="inline-block min-w-max m-6 bg-white rounded-3xl p-4 text-left">
+            <h1 className="text-xl mb-3 font-semibold">Components</h1>
+            <hr />
+            <p className="my-4 opacity-50">Drag and Drop</p>
+            <ul className="space-y-4">
+                <div className="border-2 p-[6px] rounded-lg w-52 flex justify-between items-center">
+                    <div className="flex space-x-2 items-center">
+                        <img src={inputLogo} alt="Input Logo" />
+                        <li>Input</li>
+                    </div>
+                    <img src={threeLines} alt="" />
+                </div> 
+                <div className="border-2 p-[6px] rounded-lg w-52 flex justify-between items-center">
+                    <div className="flex space-x-2 items-center">
+                        <img src={llmLogo} alt="LLM Logo" />
+                        <li>LLM Engine</li>
+                    </div>
+                    <img src={threeLines} alt="" />
+                </div> 
+                <div className="border-2 p-[6px] rounded-lg w-52 flex justify-between items-center">
+                    <div className="flex space-x-2 items-center">
+                        <img src={outputLogo} alt="Output Logo" />
+                        <li>Output</li>
+                    </div>
+                    <img src={threeLines} alt="" />
+                </div> 
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar
